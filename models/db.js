ff9a63aacf5f9494674444bc005d06d8b70f075c
xiaojuejiang <370208645@qq.com>
2018-10-21 22:23:41 +0800

    解决了数据库链接错误的问题

--- models/db.js
+++ models/db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const pool  = mysql.createPool({
     host : '127.0.0.1',
     user : 'root',
-    password : '',
+    password : 'root',
     database : 'letao'
 });
 
---
+++ public/myProject/js/category.js
@@ -0,0 +1,12 @@
+$(function () {
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    });
+    $.ajax({
+        url:'/category/queryTopCategory',
+        type:'get',
+        success:function (response) {
+            console.log(response);
+        }
+    })
+})
